c589215b017a0560eb1ce25e638840db47696730
Leander Rijneveld <leanderrijneveld@monotch.com>
2024-11-03 13:58:13 +0000

    Improved bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Install kubectl
+echo "Installing kubectl..."
 az aks install-cli
 
 # Get cluster credentials
+echo "Configuring kubectl for ${MANAGEDCLUSTERNAME} in ${RESOURCEGROUP}..."
 az aks get-credentials -g ${RESOURCEGROUP} -n ${MANAGEDCLUSTERNAME}
 
 # Install namespace as proof of kubernetes interaction
+echo "Creating namespace using kubectl..."
 kubectl create ns bootstrapped
