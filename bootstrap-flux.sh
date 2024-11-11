9506ff4c10226d74b4979409591beaf670c59759
Ruud Bleeker <ruudbleeker@monotch.com>
2024-11-11 20:38:34 +0000

    install ssh key as file, fixed github url

--- bootstrap-flux.sh
+++ bootstrap-flux.sh
@@ -6,17 +6,21 @@ az aks install-cli
 # Get cluster credentials
 az aks get-credentials -g ${RESOURCEGROUP} -n ${MANAGEDCLUSTERNAME}
 
+# Install the SSH key
+install -D -m 0600 <(printf -- "${KEY}") "${HOME}/.ssh/id_ed25519"
+
 # Install flux CLI
-curl -s https://fluxcd.io/install.sh | bash
+curl -qs https://fluxcd.io/install.sh | bash
 
 # Install flux controllers
 flux install
 
 # Bootstrap flux
 flux bootstrap git \
-  --url=ssh://git@git@github.com/monotch-research/azure-flux.git \
+  --url=ssh://git@github.com/monotch-research/azure-flux.git \
   --branch=main \
-  --private-key-file=<(echo -e "${KEY}") \
+  --private-key-file="${HOME}/.ssh/id_ed25519"
   --password="${PASS}" \
   --interval=10s \
   --path=clusters/aks
+
