41332ab4195361009b8aca71fce7b5af4d9f550f
Ruud Bleeker <ruudbleeker@monotch.com>
2024-12-05 16:39:37 +0100

    SYS-2187: deploy interchange using helm instead of flux

---
+++ bootstrap-flux-helm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install kubectl
+az aks install-cli
+
+# Get cluster credentials
+az aks get-credentials -g ${RESOURCEGROUP} -n ${MANAGEDCLUSTERNAME}
+
+# Install the SSH key
+install -D -m 0600 <(printf -- "${KEY}") "${HOME}/.ssh/id_ed25519"
+
+# Install flux CLI
+curl -qs https://fluxcd.io/install.sh | bash
+
+# Install helm CLI
+curl -qs https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Install flux controllers
+flux install
+
+# Bootstrap flux
+flux bootstrap git \
+  --url=ssh://git@github.com/monotch-research/azure-flux.git \
+  --branch=main \
+  --private-key-file="${HOME}/.ssh/id_ed25519" \
+  --password="${PASS}" \
+  --interval=10s \
+  --path=clusters/aks \
+  --silent
+
