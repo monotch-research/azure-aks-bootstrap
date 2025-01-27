7b1515aaf7dc4f253df07b219dde8a96365ef646
Ruud Bleeker <ruudbleeker@monotch.com>
2025-01-27 03:40:22 +0100

    SYS-2308: Put all the cluster IPs on the message bus

--- bootstrap-talos.sh
+++ bootstrap-talos.sh
@@ -13,7 +13,20 @@ read -r -d '' JSON <<EOF
         "subscriptionDisplayName": "${AZD_SUBSCRIPTION_DISPLAY_NAME}",
         "tenantId": "${AZD_TENANT_ID}",
         "tenantDisplayName": "${AZD_TENANT_DISPLAY_NAME}",
-        "tenantCountryCode": "${AZD_TENANT_COUNTRY_CODE}"
+        "tenantCountryCode": "${AZD_TENANT_COUNTRY_CODE}",
+        "talosClusterIP": "${AZD_CLUSTER_IP}",
+        "talosControlplane0PublicIP": "${AZD_CONTROLPLANE_0_PUBIP}",
+        "talosControlplane1PublicIP": "${AZD_CONTROLPLANE_1_PUBIP}",
+        "talosControlplane2PublicIP": "${AZD_CONTROLPLANE_2_PUBIP}",
+        "talosControlplane0PrivateIP": "${AZD_CONTROLPLANE_0_PRIVIP}",
+        "talosControlplane1PrivateIP": "${AZD_CONTROLPLANE_1_PRIVIP}",
+        "talosControlplane2PrivateIP": "${AZD_CONTROLPLANE_2_PRIVIP}",
+        "talosWorker0PublicIP": "${AZD_WORKER_0_PUBIP}",
+        "talosWorker1PublicIP": "${AZD_WORKER_1_PUBIP}",
+        "talosWorker2PublicIP": "${AZD_WORKER_2_PUBIP}",
+        "talosWorker0PrivateIP": "${AZD_WORKER_0_PRIVIP}",
+        "talosWorker1PrivateIP": "${AZD_WORKER_1_PRIVIP}",
+        "talosWorker2PrivateIP": "${AZD_WORKER_2_PRIVIP}"
     }
 }
 EOF
